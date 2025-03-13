bf9829776afd37243e48cd888d3942ee431b6dee
Vikas Bansal <vbansal@blackberry.com>
2025-03-13 18:14:57 +0530

    run time error fixed

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -4,24 +4,19 @@ import {
   Text,
   View,
 } from 'react-native';
-import {SafeAreaView, SafeAreaProvider} from 'react-native-safe-area-context';
  
 function App(): React.JSX.Element {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <View>
-          <Text selectable>This Text should be selectable but can not be selected on old architecture on android from react native version 0.77 onwards</Text>
-          <Text>|  this Text is non-selectable as it is not using selectable prop |</Text>
+        <View style={styles.container}>
+          <Text selectable>This text should be selectable but can not be selected on old architecture</Text>
         </View>
-      </SafeAreaView>
-</SafeAreaProvider>
   );
 }
  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding:20
   },
   
 });
